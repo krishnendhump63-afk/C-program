baf5c1b539854bf4c27ca9515a45cf80c683d2e9
krishnendhump63-afk <krishnendhump63@gmail.com>
2026-01-05 09:44:23 +0530

    Add Temperature.c file

---
+++ Temperature.c
@@ -0,0 +1,9 @@
+#include<stdio. h>
+void main()
+{
+   float tf, tc;
+   printf ("\n Enter temperature in Fahrenheit:);
+   scanf("%f, &tf);
+   tc=(tf-32)* 5.0/9;
+   printf("\n Temperature in celsius=%f, tc);
+   }
